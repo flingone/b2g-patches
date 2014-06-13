87832f1a0163e4087d0adaf039727d8e98c793c6
Fling One <onefling8@gmail.com>
2014-06-13 20:51:09 +0800

    patch.sh: be more quiet

--- patch.sh
+++ patch.sh
@@ -1,13 +1,13 @@
 echo =============== begin to patch source code ===============
 for i in patches/*.patch
 do
-	echo -e "\033[01;33mapply $i...\033[00m"
-	patch -f -p1 <$i
+	echo -ne "\033[01;33mapply $i...\033[00m"
+	patch -f -p1 <$i >/dev/null 2>&1
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo -e "\033[01;31mfailed\033[00m"
-		echo -e " \033[01;33mtry to reverse apply $i...\033[00m"
-		patch -f -p1 -R <$i && patch -f -p1 <$i
+		echo -ne " \033[01;33mtry to reverse apply $i...\033[00m"
+		patch -f -p1 -R <$i >/dev/null 2>&1 && patch -f -p1 <$i >/dev/null 2>&1
 		res1=$?
 		if [ $res1 -ne 0 ]; then
 			echo -e " \033[01;31mfailed, please check your patch: [$i]\033[00m"
